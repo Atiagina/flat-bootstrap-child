a15556898f853eecaa334f83da7ff3efee01bf79
Anna <atiagina@gmail.com>
2017-11-07 12:19:39 -0800

    adds sidebar

--- home_page.php
+++ home_page.php
@@ -15,9 +15,16 @@ Template Name:Home Page
  */
 
 get_header(); ?>
+<?php get_sidebar('home'); ?>
+
 <div class="container">
 <div class="row">
 <h1>I love cats. Especially my cat Kot.</h1>
+	</div>
+</div>
+<div class="container">
+<div class="row">
+<h1>Headline 2</h1>
 	</div>
 </div>
 
