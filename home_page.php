e27785fb1f81aeba68b025c2dfc7600c20e9a25c
Anna <atiagina@gmail.com>
2017-11-07 13:18:25 -0800

    adds a query

--- home_page.php
+++ home_page.php
@@ -33,4 +33,35 @@ if( function_exists('fa_display_slider') ){
 	</div>
 </div>
 
+<div class="container">
+<div class="row">
+<h2>Recent Posts</h2>
+<?php $custom_query = new
+WP_Query('posts_per_page=4&category_name=architecture');
+while($custom_query->have_posts()) : $custom_query->the_post(); ?>
+<!--before you start showing me a post, wrap it in a div with a
+bootstrap class-->
+<div class="col-md-3 push <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
+<!--get the thumbnail; you created this with 'featured image'-->
+<?php the_post_thumbnail (medium); ?>
+<!--get the post title, wrap it in an h3 tag and make it a hyper link to
+the actual post-->
+<h3><a href="<?php the_permalink(); ?>"><?php
+the_title(); ?></a></h3>
+<!--get the date. M d, Y – will output – Nov 06, 2015-->
+<?php echo '<p class="date">' . date('l, F jS') . '</p>'; ?>
+<!--get the author's name-->
+<p class="author"><?php the_author(); ?></p>
+<!--get the excerpt-->
+<?php the_excerpt(); ?>
+	</div>
+
+<?php endwhile; ?>
+<?php wp_reset_postdata(); // reset the query ?>
+
+<!--loop will go here-->
+
+</div>
+</div>
+
 <?php get_footer(); ?>
