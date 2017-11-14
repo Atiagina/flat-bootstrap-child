f8ba8360ea90b94673172d91116b661ee1a01271
Anna <atiagina@gmail.com>
2017-11-13 20:55:19 -0800

    tests

--- header.php
+++ header.php
@@ -15,6 +15,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="http://atiagina.com/fonts/Graphik-Regular.otf" rel="stylesheet">
+<link href="http://atiagina.com/fonts/Graphik-Bold.otf" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <?php wp_head(); ?>
