86a57c97bcef64793c832eb2bceea99054290e28
Anna <atiagina@gmail.com>
2017-11-20 21:20:47 -0800

    php tests

--- home_page.php
+++ home_page.php
@@ -147,8 +147,10 @@ the_title(); ?></a></h3>
 		// The Loop
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
-				$query->the_post(); 
-				the_field('ad-banner-1'); 
+				$query->the_post(); ?>
+              
+               <img src="<?php the_field('ad-banner-1'); ?> " />
+		 <?php
 			}
 		}
 		/* Restore original Post Data */
