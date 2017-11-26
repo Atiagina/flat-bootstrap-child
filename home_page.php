eb9747db839f9f8d501a056931d8517b49966476
Anna <atiagina@gmail.com>
2017-11-26 10:45:10 -0800

    php tests

--- home_page.php
+++ home_page.php
@@ -112,11 +112,13 @@ while($custom_query->have_posts()) : $custom_query->the_post(); ?>
 bootstrap class-->
 <div class="col-xs-12 col-sm-4 push sectionpost <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
 
-<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID,'thumbnail-news'));
+<?php 
+	echo get_the_post_thumbnail( $post_id, 'thumbnail', array( 'class' => 'sectionimg linebackground' ) );
+	// $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail-news') );
 				
-				echo '<img class="" src="';
-				echo $url;
-				echo '">'; ?>
+				//echo '<img class="" src="';
+				//echo $url;
+				//echo '">'; ?>
 <!--get the post title, wrap it in an h3 tag and make it a hyper link to
 the actual post-->
 <h3><a href="<?php the_permalink(); ?>"><?php
