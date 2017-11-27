dd3f1e7b6f082bd3e54a01923757882d6280e770
Anna <atiagina@gmail.com>
2017-11-27 14:06:17 -0800

    updates category headline

--- single-pictures.php
+++ single-pictures.php
@@ -10,7 +10,7 @@ get_header(); ?>
 <div class="container">
 
 <div class="row">
-<div class="col-xs-12 col-sm-4">
+<div class="col-xs-12 col-sm-4 category-headline">
 <?php the_field('category-for-headline'); ?>
 	</div>
 	</div>
