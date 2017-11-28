69c28c9163a26f7d1b2320833eb316a14956069f
Anna <atiagina@gmail.com>
2017-11-27 18:48:04 -0800

    fixes

--- single-wander.php
+++ single-wander.php
@@ -43,7 +43,7 @@ bootstrap class-->
 <div class="col-xs-12 col-sm-offset-2 col-sm-8 cdmxcard <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
 
 <?php echo '<img src="';
-	$url; 
+	echo $url; 
 	echo '" >';?>
 <!--get the post title, wrap it in an h3 tag and make it a hyper link to
 the actual post-->
