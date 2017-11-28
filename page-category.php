3ab471b248fe716cfa03e650df63aba5a7d9b63e
Anna <atiagina@gmail.com>
2017-11-28 11:15:40 -0800

    adds category page

---
+++ page-category.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Theme: Flat Bootstrap
+ * 
+ * Template Name: Page - Category
+ *
+ * Page with sidebar that displays a site index
+ *
+ * This is the template that displays a site index with search, pages, categories,
+ * tags, etc. It has a sidebar.
+ *
+ * @package flat-bootstrap
+ */
+
+get_header(); ?>
+
+
+<div class="container">
+
+<div class="row">
+<div class="col-xs-12 col-sm-4 category-headline">
+<?php
+the_title(); ?>
+	</div>
+	</div>
+</div>
+
+
+<div class="container">
+
+<div class="row">
+<div class="category-top-grid">
+
+	</div>
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
